60e06f2aec1639d4b511ecfabb0d096f1c29487f
Herman Peter Bruna <hp@brunabruna.nl>
2013-03-04 18:47:12 +0100

    werkende chat met positionering en kleuren

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = require('express').createServer();
+var io = require('socket.io').listen(app);
+
+var colorclasses = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+
+app.listen(8080);
+
+// routing
+
+ app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// usernames which are currently connected to the chat
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+	// when the client emits 'sendchat', this listens and executes
+	socket.on('sendchat', function (data, MouseXposition, MouseYposition) {
+		// we tell the client to execute 'updatechat' with 2 parameters
+		console.log(socket);
+		io.sockets.emit('updatechat', socket.username, socket.usercolor, data, MouseXposition, MouseYposition);
+	});
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function(username){
+		// we store the username in the socket session for this client
+		socket.username = username;
+		// add the client's username to the global list
+		socket.usercolor =  colorclasses.shift();
+
+		usernames[username] = username;
+		// echo to client they've connected
+		socket.emit('updatechat', 'SERVER', 'you have connected');
+		// echo globally (all clients) that a person has connected
+		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+		// update the list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function(){
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// update list of users in chat, client-side
+		io.sockets.emit('updateusers', usernames);
+		// echo globally that this client has left
+		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+	});
+});
