67643811b7cd4e98e38bda61b425521e5a6a1ff6
Herman Peter Bruna <hp@brunabruna.nl>
2013-03-15 14:39:20 +0100

    - verschillende boards - alle functies werken
    to do: idea object en user object per board

--- app.js
+++ app.js
@@ -1,74 +1,95 @@
  "use strict";
+ var express = require('express');
+ var app = require('express').createServer();
+ var io = require('socket.io').listen(app);
+ var usernames = {};
+ var ideaID = 0;
+ var ideas = [];
+ var boards = [];
 
-var express = require('express');
-var app = require('express').createServer();
-var io = require('socket.io').listen(app);
-var usernames = {};
-var ideaID = 0;
-var ideas = [];
+ //app.listen(80);
+ app.listen(8080);
+ io.set('log level', 2);
 
-//app.listen(80);
-app.listen(8080);
+ // routing
+ app.use(express.static(__dirname + '/public'));
+ app.get('/', function(req, res) {
+     res.sendfile(__dirname + '/index.html');
+ });
 
-//io.set('log level', 2);
+ io.sockets.on('connection', function(socket) {
+     // give a new connection all the existing ideas
+     var value;
+     for (value in ideas) {
+         if (ideas.hasOwnProperty(value)) {
+             socket.emit('sendNewIdea', ideas[value]);
+         }
+     }
 
-// routing
-app.use(express.static(__dirname + '/public'));
+     // new idea comes from client
+     socket.on('newIdea', function(data, mouseXposition, mouseYposition) {
+         ideaID++;
+         ideas[ideaID] = {
+             id: ideaID,
+             content: data,
+             user: socket.username,
+             xPos: mouseXposition,
+             yPos: mouseYposition,
+             likes: 0
+         };
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+         //send idea to everyone is the same room as the sender.
+         io.sockets. in (socket.room).emit('sendNewIdea', ideas[ideaID]);
+     });
 
+     // idea is updated
+     socket.on('sendUpdatedIdea', function(ideaID, xPos, yPos) {
+         ideas[ideaID].xPos = xPos; //update Idea object
+         ideas[ideaID].yPos = yPos; //update Idea object
+         io.sockets. in (socket.room).emit('updateIdea', ideaID, xPos, yPos);
+     });
 
-io.sockets.on('connection', function (socket) {
-	// give a new connection all the existing ideas
-	var value;
-	for (value in ideas) {
-    if (ideas.hasOwnProperty(value)) {
-        socket.emit('sendNewIdea', ideas[value]);}
-    }
+     // idea is liked
+     socket.on('sendUpdatedLikes', function(ideaID, increase) {
+         ideas[ideaID].likes = ideas[ideaID].likes + increase; //update Idea object
+         var likes = ideas[ideaID].likes;
+         io.sockets. in (socket.room).emit('updateLikes', ideaID, likes);
+     });
 
-	// new idea comes from client
-	socket.on('newIdea', function (data, mouseXposition, mouseYposition) {
-		ideaID++;
-		ideas[ideaID] = {id: ideaID, content: data, user: socket.username, xPos: mouseXposition, yPos: mouseYposition, likes: 0};
-		io.sockets.emit('sendNewIdea', ideas[ideaID]);
-	});
+     // idea is deleted
+     socket.on('sendDeleteIdea', function(ideaID) {
+         delete ideas[ideaID]; //delete Idea object
+         io.sockets. in (socket.room).emit('deleteIdea', ideaID);
+     });
 
-	// idea is updated
-	socket.on('sendUpdatedIdea', function(ideaID, xPos, yPos){
-		ideas[ideaID].xPos = xPos; //update Idea object
-		ideas[ideaID].yPos = yPos; //update Idea object
-		io.sockets.emit('updateIdea', ideaID, xPos, yPos);
-	});
+     // Users
+     socket.on('addUser', function(username, boardname, hash) {
+         socket.username = username;
 
-	// idea is liked
-	socket.on('sendUpdatedLikes', function(ideaID, increase){
-		ideas[ideaID].likes = ideas[ideaID].likes + increase; //update Idea object
-		var likes = ideas[ideaID].likes;
-		io.sockets.emit('updateLikes', ideaID, likes);
-	});
+         //create a board or connect to existing board
+         if (hash !== 0) {
+            console.log(hash);
+             socket.room = hash;
 
-	// idea is deleted
-	socket.on('sendDeleteIdea', function(ideaID){
-		delete ideas[ideaID]; //delete Idea object
-		io.sockets.emit('deleteIdea', ideaID);
-	});
+         } else {
+             var now = new Date().getTime();
+             socket.room = boardname + now;
 
+         }
 
+         
 
-	// Users
-	socket.on('addUser', function(username){
-		socket.username = username;
-		usernames[username] = username;
-		io.sockets.emit('updateUsers', usernames);
-	});
+         usernames[username] = username;
 
-	// when the client disconnects
-	socket.on('disconnect', function(){
-		// remove the username en update the client userlists
-		delete usernames[socket.username];
-		io.sockets.emit('updateUsers', usernames);
-		
-	});
-});
+         socket.join(socket.room);
+         io.sockets. in (socket.room).emit('updateUsers', usernames);
+     });
+
+     // when the client disconnects
+     socket.on('disconnect', function() {
+         // remove the username en update the client userlists
+         delete usernames[socket.username];
+         socket.leave(socket.room);
+         io.sockets.emit('updateUsers', usernames);
+     });
+ });
--- public/js/client.js
+++ public/js/client.js
@@ -45,7 +45,17 @@
 			$('#pagecover').fadeOut(1000);
 			$('#content').fadeIn(10000);
 
-			socket.emit('addUser', prompt("Met wie hebben we het genoegen?"));
+
+			var username = prompt("Met wie hebben we het genoegen?")
+
+			if (window.location.hash) {
+				var hash = window.location.hash;
+			} else {
+				var hash = 0;
+				var boardname = prompt("What is de title of your ideaboard");
+			}
+
+			socket.emit('addUser', username, boardname, hash);
 
 			// listeners voor server events
 
@@ -62,81 +72,81 @@
 					},
 					mouseleave: function() {
 						//controls.detach();
-				}
+					}
 				});
 
-			$(".idea").draggable({
-				start: function() {
-					// bij de start nog een mooi kleurtje?
-				},
-				drag: function() {
-					//tijdens ook nog een symbooltje?
-				},
-				stop: function() {
-					var finalposition = $(this).position();
-					var ideaID = $(this).attr('id');
-					var xPos = finalposition.left;
-					var yPos = finalposition.top;
-					sendUpdatedIdea(ideaID, xPos, yPos);
+				$(".idea").draggable({
+					start: function() {
+						// bij de start nog een mooi kleurtje?
+					},
+					drag: function() {
+						//tijdens ook nog een symbooltje?
+					},
+					stop: function() {
+						var finalposition = $(this).position();
+						var ideaID = $(this).attr('id');
+						var xPos = finalposition.left;
+						var yPos = finalposition.top;
+						sendUpdatedIdea(ideaID, xPos, yPos);
+					}
+				});
+			});
+
+			socket.on('updateIdea', function(ideaID, xPos, yPos) {
+				$('#' + ideaID).offset({
+					top: yPos,
+					left: xPos
+				});
+			});
+
+			socket.on('deleteIdea', function(ideaID) {
+				// detach idea from DOM
+				$('#' + ideaID + '.idea').detach();
+			});
+
+			socket.on('updateLikes', function(ideaID, likes) {
+				var fontsize = 100 + (likes * 30) + '%';
+				$('#' + ideaID).css('font-size', fontsize);
+			});
+
+			socket.on('updateUsers', function(data) {
+				$('#users').empty();
+				$.each(data, function(key, value) {
+					$('#users').append(key + ' ');
+				});
+			});
+
+
+			// triggers to server
+
+			sendUpdatedIdea = function(ideaID, xPos, yPos) {
+				// tell server to update idea-object with position and send updated idea to clients.
+				socket.emit('sendUpdatedIdea', ideaID, xPos, yPos);
+			};
+			sendUpdatedLikes = function(ideaID, increase) {
+				// tell server to update Likes in idea-object and send updated object to clients.
+				socket.emit('sendUpdatedLikes', ideaID, increase);
+			};
+
+
+			// typing and sending the idea
+			$(document.body).on('keydown', function() {
+				$('#ideafield').focus(); // makes that all typing is set for the idea.
+			});
+
+			$(document).mousemove(function(e) {
+				mouseXposition = e.pageX;
+				mouseYposition = e.pageY;
+			});
+
+			// when the client hits ENTER on their keyboard
+			$('#ideafield').keypress(function(e) {
+				if (e.which === 13) {
+					var idea = $('#ideafield').val();
+					$('#ideafield').val('');
+					socket.emit('newIdea', idea, mouseXposition, mouseYposition);
 				}
 			});
-			});
-
-		socket.on('updateIdea', function(ideaID, xPos, yPos) {
-			$('#' + ideaID).offset({
-				top: yPos,
-				left: xPos
-			});
-		});
-
-		socket.on('deleteIdea', function(ideaID) {
-			// detach idea from DOM
-			$('#' + ideaID + '.idea').detach();
-		});
-
-		socket.on('updateLikes', function(ideaID, likes) {
-			var fontsize = 100 + (likes * 30) + '%';
-			$('#' + ideaID).css('font-size', fontsize);
-		});
-
-		socket.on('updateUsers', function(data) {
-			$('#users').empty();
-			$.each(data, function(key, value) {
-				$('#users').append(key + ' ');
-			});
-		});
-
-
-		// triggers to server
-
-		sendUpdatedIdea = function(ideaID, xPos, yPos) {
-			// tell server to update idea-object with position and send updated idea to clients.
-			socket.emit('sendUpdatedIdea', ideaID, xPos, yPos);
-		};
-		sendUpdatedLikes = function(ideaID, increase) {
-			// tell server to update Likes in idea-object and send updated object to clients.
-			socket.emit('sendUpdatedLikes', ideaID, increase);
-		};
-
-
-		// typing and sending the idea
-		$(document.body).on('keydown', function() {
-			$('#ideafield').focus(); // makes that all typing is set for the idea.
-		});
-
-		$(document).mousemove(function(e) {
-			mouseXposition = e.pageX;
-			mouseYposition = e.pageY;
-		});
-
-		// when the client hits ENTER on their keyboard
-		$('#ideafield').keypress(function(e) {
-			if (e.which === 13) {
-				var idea = $('#ideafield').val();
-				$('#ideafield').val('');
-				socket.emit('newIdea', idea, mouseXposition, mouseYposition);
-			}
-		});
 		}); // end socket.on
 	}); // end document.ready
 	
